fecea2d8d10f901fe37668c7285e617d9256c901
Erangel12 <EmmanuelRangel@protonmail.com>
2024-11-24 23:26:01 -0600

    feature: se creo el spinner que se utilizara en la aplicacion

---
+++ src/app/components/spinner-inline/spinner-inline.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner-inline',
+  imports: [],
+  templateUrl: './spinner-inline.component.html',
+  styleUrl: './spinner-inline.component.css'
+})
+export class SpinnerInlineComponent {
+
+}
