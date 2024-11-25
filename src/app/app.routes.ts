718aa601538e198d9c24022f53b52497844e6f46
Erangel12 <EmmanuelRangel@protonmail.com>
2024-11-24 23:27:56 -0600

    feature: se configuraraon las rutas de la app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./Home/layout/layout-home/layout-home.component'),
+    children: [
+      {
+        path: 'travels',
+        title: 'Viajes',
+        loadComponent: () =>
+          import('./Home/pages/travels-page/travels-page.component'),
+      },
+      {path:'**',redirectTo:'travels'},
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
